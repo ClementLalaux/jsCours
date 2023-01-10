57254d7e2f9fd275e142c6d42ff93a51fc6b512f
Clement Lalaux <lalauxclement@gmail.com>
2023-01-10 13:51:35 +0100

    Exercice 4-5-6

--- script.js
+++ script.js
@@ -22,4 +22,32 @@ function exoTrois(){
     console.log(`Le perimetre d'un cercle de diametre ${diametre} est de ${perimetre} et l'aire est de ${aire}`);
 }
 
-exoTrois();
+//exoTrois();
+
+function exoQuatre(){
+    let cote = Number(prompt("Entrez un coté"));
+    let perimetre = cote * 4;
+    let aire = cote * cote;
+    console.log(`Le perimetre d'un carre de longueur ${cote} est de ${perimetre} et l'aire est de ${aire}`);
+}
+
+//exoQuatre();
+
+function exoCinq(){
+    let cote1 = Number(prompt("Entrez un coté du triangle"));
+    let cote2 = Number(prompt("Entrez un autre coté du triangle"));
+    let hypothenuse = Math.sqrt(Math.pow(cote1,2) + Math.pow(cote2,2)).toFixed(2);
+    console.log(`L'hypothénuse d'un triangle rectangle qui a pour côté ${cote1} et ${cote2} est de ${hypothenuse}`);
+}
+
+//exoCinq();
+
+function exoSix(){
+    let prix = Number(prompt("Entrez un prix"));
+    let tva = Number(prompt("Entrez une TVA"));
+    let prixTTC = prix + (prix*tva/100);
+    let prixTVA = prix*tva/100;
+    console.log(`Pour un objet qui coute ${prix} avec une tva égale à ${tva} , le cout de la tva appliquée à l'objet sera de ${prixTVA} et donc l'objet coutera ${prixTTC} TTC`);
+}
+
+//exoSix();
