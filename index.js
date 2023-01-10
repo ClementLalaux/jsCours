ec76c810dd3f4f0bc1c9628b1f6074e881af9612
Clement Lalaux <lalauxclement@gmail.com>
2023-01-10 11:28:55 +0100

    Exercice 2

--- index.js
+++ index.js
@@ -4,4 +4,13 @@ function exoUn(){
     console.log(`Bonjour ${nom} ${prenom}`);
 }
 
-exoUn();
+//exoUn();
+
+function exoDeux(){
+    let nbA = prompt("Ecrivez un nombre a : ");
+    let nbB = prompt("Ecrivez un nombre b : ");
+    let res = parseFloat(nbA) + parseFloat(nbB);
+    console.log(`${nbA} + ${nbB} = ${res}`);
+}
+
+exoDeux();
