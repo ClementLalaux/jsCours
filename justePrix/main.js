40e10bcde4f1ec283a67fa0d923cf08ca7c7e098
Clement lalaux <lalauxclement@gmail.com>
2023-01-19 15:37:19 +0100

    Juste prix

---
+++ justePrix/main.js
@@ -0,0 +1,16 @@
+let numAlea = Math.round(Math.random()*1000);
+let soumettre = document.querySelector('#btn-submit');
+let valeurSoumise = document.querySelector('#valeur');
+let reponse = document.querySelector('#reponse')
+
+soumettre.addEventListener('click', () => {
+
+    if(valeurSoumise.value > numAlea){
+        reponse.innerText = "Votre valeur est trop grande";
+    } else if (valeurSoumise.value < numAlea){
+        reponse.innerText = "Votre valeur est trop petite";
+    } else{
+        reponse.innerText = "Vous avez trouvé la réponse";
+    } 
+
+})
