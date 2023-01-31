67db0f3886d20f38534809e0e648f5380ba5a1c5
Clement lalaux <lalauxclement@gmail.com>
2023-01-31 11:41:00 +0100

    exo parking

--- objet/Tp parking/classe/Vehicule.js
+++ objet/Tp parking/classe/Vehicule.js
@@ -7,7 +7,47 @@ export default class Vehicule {
         this.immatriculation = immatriculation;
     }
 
+    get marque(){
+        return this.marque;
+    }
+
+    set marque(marque){
+        this.marque = marque;
+    }
+
+    get modele(){
+        return this.modele;
+    }
+
+    set modele(modele){
+        this.modele = modele;
+    }
+
+    get kilometrage(){
+        return this.kilometrage;
+    }
+
+    set kilometrage(kilometrage){
+        this.kilometrage = kilometrage;
+    }
+
+    get annee(){
+        return this.annee;
+    }
+
+    set annee(annee){
+        this.annee = annee;
+    }
+
+    get immatriculation(){
+        return this.immatriculation;
+    }
+
+    set immatriculation(immatriculation){
+        this.immatriculation = immatriculation;
+    }
+
     display(){
-        return `La voiture de type ${marque} , de modele ${modele} avec un kilometrage de ${kilometrage} et qui est sortie de concession en ${annee}`
+        return `La voiture de type ${marque()} , de modele ${modele()} avec un kilometrage de ${kilometrage()} et qui est sortie de concession en ${annee()}`
     }
 }
